bd097d74109ed2331bcba622ce3ae3ce39f0eeef
erksters <erksters@F1092-06-2094>
2019-09-16 14:24:36 -0500

    This is the updated vector3 with Magnitude

--- src/Vector3.cs
+++ src/Vector3.cs
@@ -47,5 +47,30 @@ namespace VectorMath
             this.Y = y;
             this.Z = z;
         }
+
+        /// <summary>
+        /// This is the magnitude of this Vector3
+        /// </summary>
+        public double Magnitude
+        {
+            get
+            {
+                return Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
+            }
+        }
+
+        /// <summary>
+        /// Calculates  the cross product of this vector3 and another vector3.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>The cross product</returns>
+        public Vector3 CrossProduct(Vector3 other)
+        {
+            return new Vector3(this.Y * other.Z - this.Z * other.Y,
+                                this.Z * other.X - this.X * other.Z,
+                                this.X * other.Y - this.Y * other.X);
+        }
+
     }
+
 }
